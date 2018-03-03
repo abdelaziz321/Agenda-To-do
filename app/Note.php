e01f2e5736e9fbba813db6273b6afb2a44729662
bu4ak <igorbu4@gmail.com>
2018-03-03 23:44:47 +0700

    Added the FormRequest instead of validation inside the NotesController. And the code inside the controller methods was simplified

--- app/Note.php
+++ app/Note.php
@@ -4,8 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Note
+ * @package App
+ */
 class Note extends Model
 {
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'body',
+    ];
+
+    /**
+     * belongs to user
+     */
     public function users()
     {
         $this->belongsTo('App\User');
